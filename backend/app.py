7ba81ccd1a739e2be32759c1c99140d8dceaff13
PETER KIBET BYEGON <79253969+Byekibe@users.noreply.github.com>
2024-10-05 12:17:44 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,14 @@ CORS(app)
 my_email = os.getenv("my_email")
 password = os.getenv('password_mail')
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return render_template('index.html')
+
 @app.route("/api/home")
 def home():
     return {
